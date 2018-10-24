e8f6e1a1492a79f28de0824cc9e9494a2b18cb0e
cantorethomas <cantorethomas@gmail.com>
2018-10-24 11:08:18 +0200

    Renaming

--- NESSRAquery.R
+++ NESSRAquery.R
@@ -1,14 +1,14 @@
 # NESSRA input 
+## be sure to setwd() to an empty new directory before running the script 
 
 # run this function to download files ------------------------------------------
 
-.downloadData <- function(platform, outDir, cores = 1){
+.downloadData <- function(platform,  cores = 1){
     
     plt <- getGEO(platform) # get platform data
-    GSEids <- Meta(plt)$series_id[1:2] # list of experiments
+    GSEids <- Meta(plt)$series_id # list of experiments
     plt_specs <- Table(plt) # platform specifications
     geneIds <- plt_specs$ID # platform genes 
-    
     print(paste0("Total number of experiments: ", length(GSEids)))
     
     getExpr <- function(i){ 
@@ -16,8 +16,7 @@
         IDs <<- geneIds 
         msg <- F
         if (all(rownames(expr) == IDs)) {  
-            fwrite(as.data.table(expr), file = paste0(outDir, as.character(i)), 
-                   row.names = T)
+            fwrite(as.data.table(expr), file = as.character(i), row.names = T)
             msg <- T
         }
         msg
@@ -30,7 +29,7 @@
 
 # run this function to concatenate matrices ------------------------------------
 
-.concatMatrices <- function(dir, outdir, cores = 1) {
+.concatMatrices <- function(dir, cores = 1) {
 
     files <- list.files(path = ".", all.files = F)
     
@@ -40,14 +39,14 @@
 
 # wrapper ----------------------------------------------------------------------
 
-downloadNessra <- function(platform, dir, cores) {
+NESSRAquery <- function(platform, cores = 1) {
     
     require(data.table)
     require(parallel)
     require(GEOquery)
     
     print(paste0("Starting downloading data at ", Sys.time()))
-    .downloadData(platform = platform, cores = cores, outDir = dir)
+    .downloadData(platform = platform, cores = cores)
     print("Concatenating many data, will require time and memory.")
     .concatMatrices(cores = cores)
     print(paste0("Done! At ", Sys.time()))
