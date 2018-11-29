61528e61badc3bdc40d381cc6a2eea3221159bc3
cantorethomas <cantorethomas@gmail.com>
2018-11-29 17:32:07 +0100

    Reordered project and Smith Waterman

--- Arabidopsis_Expression_Dataset/DataCleaning.R
+++ Arabidopsis_Expression_Dataset/DataCleaning.R
@@ -37,14 +37,16 @@ NESSRAdata <- NESSRAdata[ , ..not_na] # cleaning
 # About characheter or whatever 
 char_count <- sapply(NESSRAdata, function(y) sum(length(which(is.character(y)))))
 char_ind <- which(char_count != 0)
-not_char <- colnames(NESSRAdata)[-na_ind]
+not_char <- colnames(NESSRAdata)[-char_ind]
 NESSRAdata <- NESSRAdata[, ..not_char]
 
 # Normalization or whatever you want to call it --------------------------------
 
-# Blanzieri suggestion 
+data_log <- log(NESSRAdata)
+
+# suggestion 
 mean_vect <- as.numeric(mclapply(NESSRAdata, mean, mc.cores = 2))
-hist(mean_vect) # he wants to cut out tail 
+hist(log(mean_vect+1), col = "blue", border = "white") # he wants to cut out tail 
 summary(mean_vect)
 
 # cutting 250 mean(exp)
@@ -59,5 +61,23 @@ boxplot(mean_vect)
 boxplot(new_mean_vect)
 
 fwrite(NESSRAdata, file = "./Norm_data")
+
+# Check log tranform 
+
+checkLog <- function(sample) {
+    qx <- as.numeric(stats::quantile(sample,
+                                     c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm = TRUE))
+    logTransformed <- !((qx[5] > 100) || (qx[6]-qx[1] > 50 && qx[2] > 0) ||
+                            (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2))
+    logTransformed 
+}
+
+logScaled <- apply(NESSRAdata, 2, checkLog)
+sum(logScaled)
+
+NESSRAdata_log <- NESSRAdata <- NESSRAdata[, ..logScaled]
+mean_log_vect <- apply(NESSRAdata, 2, mean)
+hist(mean_log_vect) # plot the mean log vect 
+
 
 
---
+++ FOF/FOF.py
@@ -0,0 +1,8 @@
+from Accessors.SmithWaterman import SmithWaterman 
+
+# example of SW call 
+seq1 = "ATCTATCTATGGGATGCTAGCTTAGTGG"
+seq2 = "AGCCTTAGCGTGATCGGGAGGCTTAGTG"
+scores = [2,-3,2,4] # Match, Mismatch, Gap, GapExt
+par_file = "./Accessors/params_data.csv"
+print(SmithWaterman(seq1, seq2, scores, par_file))
